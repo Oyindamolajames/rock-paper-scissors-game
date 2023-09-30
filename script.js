f031479446745c0718904bf594e4917cc2d74623
Ogunsanya James <oyindamolajames@gmail.com>
2023-09-30 11:44:40 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+let winMsg = "Victory";
+let loseMsg = 'Crushing Defeat';
+let tieMsg = 'Tie';
+let moveList = ['Rock', 'Paper', 'Scissors'];
+let statusDisplay = document.querySelector('#status-head');
+let moveDisplays = document.querySelectorAll('.move-display h2');
+let buttons = document.querySelectorAll('button');
+
+
+/**
+ * Computes result of the game. returns victory message if move 1 wins.
+ * @param  {Number}   move1  move 1
+ * @param  {Number}   move2  move 2
+ * @return {String}   result result of the game
+ */
+
+let calcResult = (move1, move2) => {
+    let result = move1 - move2;
+
+    if (result == 1 || result + 3 == 1){
+        return winMsg;    
+    } else if (result == 2 || result + 3 == 2){
+        return loseMsg;
+    } else {
+        return tieMsg;
+    }
+}
+
+/**
+ * Generates random number
+ * @return {Number} random number between 0 and 2
+ */
+
+let randomNumber = () => {
+    return Math.floor(Math.random() * 3);
+}
+
+/**
+ * Displays start state of game
+ */
+
+let startGame = () => {
+    statusDisplay.textContent = "Choose!";
+
+    buttons.forEach((button, index) => {
+        button.textContent = moveList[index];
+        button.style.display = "inline-block"
+        buttons[index].addEventListener("click", endGame);
+    })
+
+    moveDisplays.forEach((move => move.style.display = 'none'));
+}
+
+/**
+ * Displays end state of game
+ * @param {Event} event event containing information of users input.
+ */
+
+let endGame = (event) => {
+    let playerMove  = moveList.indexOf(event.target.textContent);
+    let computerMove = randomNumber()
+
+    statusDisplay.textContent = calcResult(playerMove, computerMove);
+
+    moveDisplays.forEach(move => move.style.display = "inline-block");
+
+    moveDisplays[0].textContent = `You played ${moveList[playerMove]}`;
+    moveDisplays[1].textContent = `Computer played ${moveList[computerMove]}`;
+
+    buttons.forEach((button, index) => {
+        if (index == 0) {
+          button.textContent = "Play Again";
+          button.removeEventListener("click", endGame);
+          button.addEventListener("click", startGame);
+        } else {
+          button.style.display = "none";
+        }
+    });
+}
+
+
+startGame();
